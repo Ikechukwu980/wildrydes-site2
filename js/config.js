630f183cfe8fae1674dcbdde088014bc0845d0fd
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-09-04 12:58:32 -0500

    Update config.js
    corrected the client ID

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_yq2BVfl1R', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6c1r6m1altkvo0cj1fm1oot7c clinet', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6c1r6m1altkvo0cj1fm1oot7c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
