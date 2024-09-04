f02ed5434a311f6f1ee0138c091639fa969869d5
Ikechukwu <106882590+Ikechukwu980@users.noreply.github.com>
2024-09-04 12:54:01 -0500

    Update config.js
    added aws cognito cofig

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_yq2BVfl1R', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6c1r6m1altkvo0cj1fm1oot7c clinet', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
